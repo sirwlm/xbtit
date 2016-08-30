0e5e235ad19e4534905e4004d02715604c80297f
KingCobra58 <kingcobra@xbtitfm.com>
2016-08-30 16:58:44 -0400

    updated  translated turkish language files

---
+++ language/turkish/lang_viewnews.php
@@ -0,0 +1,10 @@
+<?php
+
+// VIEWNEWS.PHP LANGUAGE FILE
+
+$language['POSTED_BY']   = 'Yazar';
+$language['POSTED_DATE'] = 'Tarih';
+$language['TITLE']       = 'Başlık';
+$language['ADD']         = 'Ekle';
+
+?>
